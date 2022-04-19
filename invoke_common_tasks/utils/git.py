3bdfd150b4ae2d43d8bee268e6eee9207aec5c0b
Josh Peak <neozenith.dev@gmail.com>
2022-04-19 12:10:10 +1000

    Add backwards compatability for git branch helper (#7)
    Co-authored-by: Josh Peak <josh.peak@86400.com.au>

--- invoke_common_tasks/utils/git.py
+++ invoke_common_tasks/utils/git.py
@@ -8,4 +8,7 @@ from invoke import run
 @lru_cache(maxsize=None)
 def git_current_branch() -> str:
     """Get the current git branch."""
-    return run("git branch --show-current", hide=True).stdout.strip()
+    #  return run("git branch --show-current", hide=True).stdout.strip()
+    # git 2.22+ supports the above command but to get backwards compatability need to use the below command.
+    # https://stackoverflow.com/a/6245587/622276
+    return run("git rev-parse --abbrev-ref HEAD", hide=True).stdout.strip()
